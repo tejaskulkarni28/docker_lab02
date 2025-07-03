2114b299444936973b8fc3b53d722145c2932214
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2025-07-03 23:56:13 +0530

    Deleted client and server folders

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res)=>{
-    res.send("Hello node.js!")
-})
-app.listen(3001, (req, res)=>{
-    console.log("Node started on port 3001")
-});
